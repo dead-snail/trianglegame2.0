d58e6daab604713974917a251bd9dd5831fa3618
SPONexe <iamaspoon67@gmail.com>
2022-06-01 01:49:04 +0100

    restart

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 
 # trianglegame2.0
 
+
 A clone of youtuber Dani's Triangle Game but made without a game engine.
 ___
 
--- include/Game/settings.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "raylib.h"
-
-namespace Settings{
-    constexpr inline int screenWidth = 800;
-    constexpr inline int screenHeight = 600;
-    constexpr inline int targetFPS = 60;
-}
